e242808fa573681897b52ba54880e1db81226807
woshishiq1 <90903172+woshishiq1@users.noreply.github.com>
2026-04-22 13:21:54 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3192,7 +3192,7 @@ async function subHtml(request, hostLength = 0, FileName, subProtocol, subConver
             const base64Encoded = btoa(subscriptionLink);
             
             // 发送POST请求到短链接服务
-            fetch('https://你的域名.pages.dev/', {
+            fetch('https://url-shorten-worker-8a9.pages.dev/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
